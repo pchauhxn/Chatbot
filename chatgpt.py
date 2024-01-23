e605440b6e5cb167d53ff3f0eab7f7b3262f75f6
pchauhxn <101443722+pchauhxn@users.noreply.github.com>
2024-01-23 16:39:55 +0530

    changes

--- chatgpt.py
+++ chatgpt.py
@@ -4,11 +4,10 @@ def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
-
 openai.api_key = open_file('openaiapikey.txt')
 completion = openai.Completion()
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['BOT:', 'USER:']):
+def gpt3_completion(prompt, engine='gpt-3.5-turbo-instruct-0914', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['BOT:', 'USER:']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
